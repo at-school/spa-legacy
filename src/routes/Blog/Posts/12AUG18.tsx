651154a0ad372a1c4845ad4e92233443ba41f726
Charl Kruger <charl.kruger@me.com>
2018-08-10 12:17:22 +1000

    bog stuff

---
+++ src/routes/Blog/Posts/12AUG18.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const ONE = () => (
+	<h1>post</h1>
+)
+
+export default ONE
--- src/routes/Blog/index.tsx
+++ src/routes/Blog/index.tsx
@@ -1,9 +1,49 @@
-import React, { Component } from 'react'
+import React, { Component, Fragment } from 'react';
+import './index.css';
+import ONE from './Posts/12AUG18';
 
-export class Blog extends Component {
-	render() {
+interface IState {
+	search: string
+	focus: boolean
+}
+
+export default class Blog extends Component<{}, IState> {
+
+	public state = {
+		search: '',
+		focus: false
+	}
+
+	public updateValue = (e: React.ChangeEvent<HTMLInputElement>) => {
+		this.setState({'search' : e.target.value})
+	}
+
+	public handleFocus = () => {
+		console.log(this.state.focus)
+		this.setState((prevState) => ({
+			focus: !prevState.focus
+		}));
+	}
+
+	public render() {
 		return (
-			<h1>Blog</h1>
+			<Fragment>
+				<div className='header'>
+					<span>Atschool Blog Post</span>
+					<div className={'search'+( this.state.focus ? ' focus': '' )}>
+						<i className='fas fa-search' />
+						<input
+							placeholder='Search posts'
+							onChange={this.updateValue}
+							onFocus={this.handleFocus}
+							onBlur={this.handleFocus}
+						/>
+					</div>
+				</div>
+				<div>
+					<ONE />
+				</div>
+			</Fragment>
 		)
 	}
 }
