f1a7b0b25f16ae82ab210a6d9f1bd9659ff502b5
Charl Kruger <charl.kruger@me.com>
2018-09-25 21:30:12 +1000

    added email route in teacher

--- src/layouts/AppLayout/TeacherLayout/Navigation/index.tsx
+++ src/layouts/AppLayout/TeacherLayout/Navigation/index.tsx
@@ -3,44 +3,53 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 const Navigation: React.SFC<{ pathname: string }> = ({ pathname }) => {
-  let menuKey = "";
+	let menuKey = "";
 
-  if (pathname.includes("/teacher/dashboard")) {
-    menuKey = "1";
-  } else if (pathname.includes("/teacher/classroom")) {
-    menuKey = "2";
-  } else if (pathname.includes("/teacher/rollcall")) {
-    menuKey = "3";
-  } else if (pathname.includes("/teacher/messages")) {
-    menuKey = "4";
-  }
-  return (
-    <Menu theme="dark" mode="inline" selectedKeys={[menuKey]}>
-      <Menu.Item key="1">
-        <Link to="/teacher/dashboard">
-          <Icon type="dashboard" />
-          <span className="nav-text">Dashboard</span>
-        </Link>
-      </Menu.Item>
-      <Menu.Item key="2">
-        <Link to="/teacher/classroom">
-          <Icon type="profile" />
-          <span className="nav-text">Classroom</span>
-        </Link>
-      </Menu.Item>
-      <Menu.Item key="3">
-        <Link to="/teacher/rollcall">
-          <Icon type="video-camera" />
-          <span className="nav-text">Roll Call</span>
-        </Link>
-      </Menu.Item>
-      <Menu.Item key="4">
-        <Link to="/teacher/messages">
-          <Icon type="message" />
-          <span className="nav-text">Messages</span>
-        </Link>
-      </Menu.Item>
-    </Menu>
+	if (pathname.includes("/teacher/dashboard"))
+		{
+		menuKey = "1";
+		} else if (pathname.includes("/teacher/classroom")) {
+			menuKey = "2";
+		} else if (pathname.includes("/teacher/rollcall")) {
+			menuKey = "3";
+		} else if (pathname.includes("/teacher/messages")) {
+			menuKey = "4";
+		} else if (pathname.includes("/teacher/email")) {
+			menuKey = "5";
+		}
+	return (
+	<Menu theme="dark" mode="inline" selectedKeys={[menuKey]}>
+		<Menu.Item key="1">
+			<Link to="/teacher/dashboard">
+			<Icon type="dashboard" />
+			<span className="nav-text">Dashboard</span>
+			</Link>
+		</Menu.Item>
+		<Menu.Item key="2">
+			<Link to="/teacher/classroom">
+			<Icon type="profile" />
+			<span className="nav-text">Classroom</span>
+			</Link>
+		</Menu.Item>
+		<Menu.Item key="3">
+			<Link to="/teacher/rollcall">
+			<Icon type="video-camera" />
+			<span className="nav-text">Roll Call</span>
+			</Link>
+		</Menu.Item>
+		<Menu.Item key="4">
+			<Link to="/teacher/messages">
+			<Icon type="message" />
+			<span className="nav-text">Messages</span>
+			</Link>
+		</Menu.Item>
+		<Menu.Item key="5">
+			<Link to="/teacher/email">
+			<Icon type="mail" />
+			<span className="nav-text">Email</span>
+			</Link>
+		</Menu.Item>
+	</Menu>
   );
 };
 
--- src/routes/Teacher/index.tsx
+++ src/routes/Teacher/index.tsx
@@ -11,6 +11,7 @@ import createTeacherLayout from "../../layouts/AppLayout/TeacherLayout";
 import ClassDetails from "./Classroom/ClassDetails";
 import ClassroomScreenRoot from "./Classroom/RootScreen";
 import Dashboard from "./Dashboard";
+import Email from "./Email"
 import Messages from "./Messages";
 import { getChatRoomIdQuery } from "./Messages/queries/queries";
 import {
@@ -25,228 +26,241 @@ class Content extends React.Component<any, any> {
   public messageSocket: any;
 
   public state = {
-    selectedRoom: null,
-    classroomId: "",
-    classroomName: "",
-    classroomDescription: "string",
-    classroomAvatar: "",
-    classroomLine: "",
-    classroomStudents: [],
-    currentMoment: {
-      day: moment(),
-      line: undefined
-    },
-    schedule: [],
-    currentClassId: "",
-    numAddDays: 0,
-    loading: false
+	selectedRoom: null,
+	classroomId: "",
+	classroomName: "",
+	classroomDescription: "string",
+	classroomAvatar: "",
+	classroomLine: "",
+	classroomStudents: [],
+	currentMoment: {
+	  day: moment(),
+	  line: undefined
+	},
+	schedule: [],
+	currentClassId: "",
+	numAddDays: 0,
+	loading: false
   };
 
   public componentDidMount() {
-    this.messageSocket = io.connect(
-      "http://127.0.0.1:5000/message",
-      {
-        transportOptions: {
-          polling: {
-            extraHeaders: {
-              Authorization: `Bearer ${this.props.token}`
-            }
-          }
-        }
-      }
-    );
-    this.messageSocket.on("connect", () => {
-      // subscribe listener to all chatrooms
-      this.props.client
-        .query({
-          query: getChatRoomIdQuery,
-          variables: { username: this.props.username }
-        })
-        .then((res: any) => {
-          try {
-            return res.data.user[0].chatrooms;
-          } catch (err) {
-            console.log(err);
-            return [];
-          }
-        })
-        .then((chatrooms: any) => {
-          console.log(chatrooms);
-          if (chatrooms) {
-            for (const chatroom of chatrooms) {
-              this.messageSocket.emit("sendMessage", {
-                chatroomId: chatroom.Id
-              });
-            }
-          }
-        });
-    });
+	this.messageSocket = io.connect(
+	  "http://127.0.0.1:5000/message",
+	  {
+		transportOptions: {
+		  polling: {
+			extraHeaders: {
+			  Authorization: `Bearer ${this.props.token}`
+			}
+		  }
+		}
+	  }
+	);
+	this.messageSocket.on("connect", () => {
+	  // subscribe listener to all chatrooms
+	  this.props.client
+		.query({
+		  query: getChatRoomIdQuery,
+		  variables: { username: this.props.username }
+		})
+		.then((res: any) => {
+		  try {
+			return res.data.user[0].chatrooms;
+		  } catch (err) {
+			console.log(err);
+			return [];
+		  }
+		})
+		.then((chatrooms: any) => {
+		  console.log(chatrooms);
+		  if (chatrooms) {
+			for (const chatroom of chatrooms) {
+			  this.messageSocket.emit("sendMessage", {
+				chatroomId: chatroom.Id
+			  });
+			}
+		  }
+		});
+	});
   }
 
   public componentDidUpdate() {
-    if (!this.props.token) {
-      this.props.history.push("/authentication/signin");
-    }
+	if (!this.props.token) {
+	  this.props.history.push("/authentication/signin");
+	}
   }
 
   public shouldComponentUpdate() {
-    return true;
+	return true;
   }
 
   public render() {
-    console.log(this.props);
-    let studentList = [];
-    let scheduleId = "";
-    let classId = "";
-    if (this.props.getScheduleDetailsQuery) {
-      const {
-        getScheduleDetailsQuery: { scheduleDetails }
-      } = this.props;
-      if (scheduleDetails && scheduleDetails.students) {
-        studentList = scheduleDetails.students;
-        scheduleId = scheduleDetails.Id;
-      }
-    }
-    if (
-      this.props.getClassQuery &&
-      this.props.getClassQuery.classroom &&
-      this.props.getClassQuery.classroom.length === 1
-    ) {
-      classId = this.props.getClassQuery.classroom[0].Id;
-    }
-    return (
-      <TeacherMessageSocket.Provider value={{ socket: this.messageSocket }}>
-        <ClassroomContext.Provider
-          value={{
-            Id: this.state.classroomId,
-            scheduleId,
-            avatar: this.state.classroomAvatar,
-            name: this.state.classroomName,
-            description: this.state.classroomDescription,
-            line: this.props.getScheduleQuery.loading
-              ? ""
-              : this.props.getScheduleQuery.latestLine.line,
-            students: studentList,
-            schedule: this.props.getAllScheduleQuery.loading
-              ? []
-              : this.props.getAllScheduleQuery.schedule,
-            classId,
-            getClassInfo: this.saveClassId
-          }}
-        >
-          <Route
-            exact={true}
-            path={"/teacher/dashboard"}
-            component={Dashboard}
-          />
-          <Route
-            exact={true}
-            path={"/teacher/classroom"}
-            component={ClassroomScreenRoot}
-          />
-          <Route
-            exact={true}
-            path="/teacher/classroom/:id/:item"
-            component={ClassDetails}
-          />
-          <Route exact={true} path={"/teacher/rollcall"} component={RollCall} />
-          <Route exact={true} path={"/teacher/messages"} component={Messages} />
-          {!this.props.history.location.pathname.includes("messages") && (
-            <ChatWindow />
-          )}
-        </ClassroomContext.Provider>
-      </TeacherMessageSocket.Provider>
-    );
+	console.log(this.props);
+	let studentList = [];
+	let scheduleId = "";
+	let classId = "";
+	if (this.props.getScheduleDetailsQuery) {
+	  const {
+		getScheduleDetailsQuery: { scheduleDetails }
+	  } = this.props;
+	  if (scheduleDetails && scheduleDetails.students) {
+		studentList = scheduleDetails.students;
+		scheduleId = scheduleDetails.Id;
+	  }
+	}
+	if (
+	  this.props.getClassQuery &&
+	  this.props.getClassQuery.classroom &&
+	  this.props.getClassQuery.classroom.length === 1
+	) {
+	  classId = this.props.getClassQuery.classroom[0].Id;
+	}
+	return (
+		<TeacherMessageSocket.Provider value={{ socket: this.messageSocket }}>
+		<ClassroomContext.Provider
+			value={{
+				Id: this.state.classroomId,
+				scheduleId,
+				avatar: this.state.classroomAvatar,
+				name: this.state.classroomName,
+				description: this.state.classroomDescription,
+				line: this.props.getScheduleQuery.loading
+					? ""
+					: this.props.getScheduleQuery.latestLine.line,
+				students: studentList,
+				schedule: this.props.getAllScheduleQuery.loading
+					? []
+					: this.props.getAllScheduleQuery.schedule,
+				classId,
+				getClassInfo: this.saveClassId
+			}}
+		>
+			<Route
+				exact={true}
+				path={"/teacher/dashboard"}
+				component={Dashboard}
+			/>
+			<Route
+				exact={true}
+				path={"/teacher/classroom"}
+				component={ClassroomScreenRoot}
+			/>
+			<Route
+				exact={true}
+				path="/teacher/classroom/:id/:item"
+				component={ClassDetails}
+			/>
+			<Route
+				exact={true}
+				path={"/teacher/rollcall"}
+				component={RollCall}
+			/>
+			<Route
+				exact={true}
+				path={"/teacher/messages"}
+				component={Messages}
+			/>
+			<Route
+				exact={true}
+				path={"/teacher/email"}
+				component={Email}
+			/>
+			{!this.props.history.location.pathname.includes("messages") && (
+				<ChatWindow />
+			)}
+		</ClassroomContext.Provider>
+		</TeacherMessageSocket.Provider>
+	);
   }
 
   public saveClassId = (currentClassId: string) => {
-    this.setState({ currentClassId });
+	this.setState({ currentClassId });
   };
 }
 // day: moment().format("dddd")
 const ContentWithApollo = compose(
   graphql(getScheduleQuery, {
-    name: "getScheduleQuery"
+	name: "getScheduleQuery"
   }),
   graphql(getAllScheduleQuery, {
-    name: "getAllScheduleQuery",
-    options: (props: any) => {
-      const scheduleData =
-        !props.getScheduleQuery.loading && props.getScheduleQuery.latestLine;
-      if (scheduleData) {
-        const current = moment();
-        const startTime = moment(scheduleData.startTime);
-        const endTime = moment(scheduleData.endTime);
-        if (current.isBefore(startTime)) {
-          console.log(current.diff(startTime, "milliseconds"));
-          props.getScheduleQuery.startPolling(
-            Math.abs(current.diff(startTime, "milliseconds"))
-          );
-        } else {
-          props.getScheduleQuery.startPolling(
-            Math.abs(current.diff(endTime, "milliseconds"))
-          );
-        }
-      }
-      return {
-        variables: {
-          day: scheduleData && moment(scheduleData.startTime).format("dddd")
-        },
-        skip: props.getScheduleQuery.loading
-      };
-    }
+	name: "getAllScheduleQuery",
+	options: (props: any) => {
+	  const scheduleData =
+		!props.getScheduleQuery.loading && props.getScheduleQuery.latestLine;
+	  if (scheduleData) {
+		const current = moment();
+		const startTime = moment(scheduleData.startTime);
+		const endTime = moment(scheduleData.endTime);
+		if (current.isBefore(startTime)) {
+		  console.log(current.diff(startTime, "milliseconds"));
+		  props.getScheduleQuery.startPolling(
+			Math.abs(current.diff(startTime, "milliseconds"))
+		  );
+		} else {
+		  props.getScheduleQuery.startPolling(
+			Math.abs(current.diff(endTime, "milliseconds"))
+		  );
+		}
+	  }
+	  return {
+		variables: {
+		  day: scheduleData && moment(scheduleData.startTime).format("dddd")
+		},
+		skip: props.getScheduleQuery.loading
+	  };
+	}
   }),
   graphql(getClassQueryByLine, {
-    name: "getClassQuery",
-    options: (props: any) => {
-      return {
-        variables: {
-          teacherUsername: props.username,
-          lineId:
-            props.getScheduleQuery.latestLine &&
-            props.getScheduleQuery.latestLine.line
-        },
-        skip: props.getScheduleQuery.loading
-      };
-    }
+	name: "getClassQuery",
+	options: (props: any) => {
+	  return {
+		variables: {
+		  teacherUsername: props.username,
+		  lineId:
+			props.getScheduleQuery.latestLine &&
+			props.getScheduleQuery.latestLine.line
+		},
+		skip: props.getScheduleQuery.loading
+	  };
+	}
   }),
   graphql(getScheduleDetailsQuery, {
-    name: "getScheduleDetailsQuery",
-    options: (props: any) => {
-      let classId = "";
-      if (
-        props.getClassQuery &&
-        props.getClassQuery.classroom &&
-        props.getClassQuery.classroom.length === 1
-      ) {
-        classId = props.getClassQuery.classroom[0].Id;
-      }
-      return {
-        variables: {
-          teacherUsername: props.username,
-          line:
-            props.getScheduleQuery.latestLine &&
-            props.getScheduleQuery.latestLine.line,
-          classId
-        }
-      };
-    },
-    skip: props =>
-      !props.getClassQuery ||
-      !props.getClassQuery.classroom ||
-      props.getClassQuery.classroom.length === 0
+	name: "getScheduleDetailsQuery",
+	options: (props: any) => {
+	  let classId = "";
+	  if (
+		props.getClassQuery &&
+		props.getClassQuery.classroom &&
+		props.getClassQuery.classroom.length === 1
+	  ) {
+		classId = props.getClassQuery.classroom[0].Id;
+	  }
+	  return {
+		variables: {
+		  teacherUsername: props.username,
+		  line:
+			props.getScheduleQuery.latestLine &&
+			props.getScheduleQuery.latestLine.line,
+		  classId
+		}
+	  };
+	},
+	skip: props =>
+	  !props.getClassQuery ||
+	  !props.getClassQuery.classroom ||
+	  props.getClassQuery.classroom.length === 0
   })
 )(withApollo(Content)) as any;
 
 const ContentWithContext = ({ history }: any) => (
   <AppContext.Consumer>
-    {value => (
-      <ContentWithApollo
-        username={value.username}
-        token={value.token}
-        history={history}
-      />
-    )}
+	{value => (
+	  <ContentWithApollo
+		username={value.username}
+		token={value.token}
+		history={history}
+	  />
+	)}
   </AppContext.Consumer>
 );
 
