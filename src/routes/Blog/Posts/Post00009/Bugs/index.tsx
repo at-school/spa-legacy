3aa7c10a5d13350881cc5a2bdd5cd39cf94b99c9
Anh Pham <phamduyanh249@live.com>
2018-10-13 15:44:29 +1100

    Recover week 9 bugs post

---
+++ src/routes/Blog/Posts/Post00009/Bugs/index.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import createSection from "../../HOC/createSection";
+const Bugs = () => {
+  return (
+    <ul>
+      <li>
+        Fixed PyMail recursive function to work for all email types, after being
+        tested with over a thousand unique emails.
+      </li>
+      <li>
+        Made computer vision instantaneous by changing the location of the image
+        encodings from the database to local files.
+      </li>
+    </ul>
+  );
+};
+export default createSection("Bugs fixed", Bugs);
